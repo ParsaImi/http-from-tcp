37c8ed116e39b536fbd2e7af5f28916ff34709d9
ParsaImi <parsaemani17@gmail.com>
2026-02-02 13:47:57 +0330

    CH1, L5

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -12,21 +13,48 @@ func main() {
 		fmt.Println(err)
 	}
 
-	line := ""
-	storage := make([]byte, 8)
-	for {
-		n, err := f.Read(storage)
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		data := storage[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			line += string(storage[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", line)
-			line = ""
-		}
-		line += string(data)
+	lines := getLinesFromReader(f)
+	for line := range lines {
+		fmt.Println(line)
 	}
+}
+
+func getLinesFromReader(f io.ReadCloser) <-chan string {
+	ch := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+		line := ""
+		storage := make([]byte, 8)
+		for {
+			n, err := f.Read(storage)
+
+			if n > 0 {
+				data := storage[:n]
+				// Process all newlines in the chunk
+				for i := bytes.IndexByte(data, '\n'); i != -1; i = bytes.IndexByte(data, '\n') { // this is a magic for loop fr
+					line += string(data[:i])
+					ch <- line
+					line = ""
+					data = data[i+1:]
+				}
+				// Append remaining part of the chunk to the line
+				line += string(data)
+			}
+
+			if err != nil {
+				if err == io.EOF {
+					break // End of file
+				}
+				fmt.Println(err) // Other errors
+				break
+			}
+		}
+		// Send the last line if it's not empty
+		if len(line) > 0 {
+			ch <- line
+		}
+	}()
+	return ch
+
 }
